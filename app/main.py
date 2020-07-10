52f1d8b3802368e95e6c6e9ea58bf3c746661b58
Hilton W. Silva <hilton.silva@qualirede.com.br>
2020-07-10 00:52:24 -0300

    .gitignore

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from pynubank import Nubank
-from datetime import datetime, timedelta
 import pandas as pd
 import json
 import flask
@@ -8,27 +7,33 @@ import os
 nu = Nubank()
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
+#Variaveis de ambiente
 NUUSER = os.environ.get('NUUSER')
 NUPASS = os.environ.get('NUPASS')
 NUCERT = os.environ.get('NUCERT')
 
+#Autenticar com certificado
 nu.authenticate_with_cert(NUUSER, NUPASS, NUCERT)
 
 def get_bill():
     # Recupera as compras feitas no cartão
     transactions = nu.get_bills()
 
+    # Filtra pela fatura aberta (atual)
     filter_bill = [i for i in transactions if (i.get('state') == 'open')]
 
     currentBillDetails = nu.get_bill_details(filter_bill[0])
 
     transactions = currentBillDetails.get('bill').get("line_items")
     transactions_json = json.dumps(transactions)
-    # Agrupa pelo campo "title" que é a categoria e soma os valores
+    
     df = pd.read_json(transactions_json)
-    #df.to_csv()
-    #print(df.to_csv())
+    
     return df.to_csv()
+
+def get_balance():
+    # Saldo da nuconta
+    return "balance,"+str(nu.get_account_balance()).replace(".","")
 
 @app.route('/card', methods=['GET'])
 def get_card():
@@ -36,4 +41,4 @@ def get_card():
 
 @app.route('/account/balance', methods=['GET'])
 def get_account_balance():
-    return "balance,"+str(nu.get_account_balance()).replace(".","")
+    return get_balance
